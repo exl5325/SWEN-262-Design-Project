182839a49d0cccc736924e0ef6ff9c1afb258fde
cxc8170 <cxc8170@rit.edu>
2017-10-12 19:28:57 -0400

    Added search functionality and storage to AirportDB

--- src/AirportDB.java
+++ src/AirportDB.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+
 /**
  * Created by calvinclark on 10/8/17.
  */
 public class AirportDB {
+    private HashMap<String, Airport> airports;
+    public AirportDB(){
+        airports = new HashMap<>();
+    }
+    public Airport findAirport(String code){
+        return airports.get(code);
+    }
 }
