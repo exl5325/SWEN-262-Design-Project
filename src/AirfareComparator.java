07d654c8ee27f90b441677869b4910eddd585c1c
Eric <exl5325@rit.edu>
2017-10-15 17:46:54 -0400

    CSV Writer + Comments

--- src/AirfareComparator.java
+++ src/AirfareComparator.java
@@ -1,10 +1,18 @@
 import java.util.Comparator;
 
 /**
- * Created by Eric on 10/9/2017.
+ * Airfare comparator class
  */
 public class AirfareComparator implements Comparator<FlightInterface> {
 
+    /**
+     * Compares two flights' airfare
+     * @param o1 flight 1
+     * @param o2 flight 2
+     * @return 1 if flight 1 > flight 2,
+     *  0 if flight 1 == flight 2
+     *  -1 if flight 1 < flight 2
+     */
     @Override
     public int compare(FlightInterface o1, FlightInterface o2) {
         if(o1.getAirfare() > o2.getAirfare()){
