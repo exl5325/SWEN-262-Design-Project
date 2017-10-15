4d7d094fa7233ae01a2444f471794a0680f22e05
cxc8170 <cxc8170@rit.edu>
2017-10-15 18:08:42 -0400

    Added comments to Reservation, ReservationDB, FlightInterface

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,28 +1,67 @@
 /**
+ * Contains a passenger's reservation for a specified itinerary.  Can retrieve data about the passenger or itinerary.
+ *
  * Created by calvinclark on 10/8/17.
  */
 public class Reservation {
     private String passenger;
     private Itinerary itinerary;
 
+    /**
+     * Holds a passenger name and an itinerary.
+     *
+     * @param passenger the passenger's name making the itinerary
+     * @param itinerary the itinerary the passenger is making a reservation for
+     */
     public Reservation(String passenger, Itinerary itinerary) {
         this.passenger = passenger;
         this.itinerary = itinerary;
     }
 
-    public String getPassenger(){ return passenger; }
+    /**
+     * Retrieves the passenger's name who created the reservation.
+     *
+     * @return passenger name
+     */
+    public String getPassenger(){
+        return passenger;
+    }
 
+    /**
+     * Retrieves the itinerary contained within the reservation.
+     *
+     * @return itinerary the reservation was made for
+     */
     public Itinerary getItinerary(){
         return itinerary;
     }
 
+    /**
+     * Retrieves the origin airport for the reservation's itinerary.
+     *
+     * @return 3-letter airport code for origin airport
+     */
     public String getOrigin() {
         return itinerary.getOrigin();
     }
 
+    /**
+     * Retrieves the destination airport for the reservation's itinerary.
+     *
+     * @return 3-letter airport code for destination airport
+     */
     public String getDestination() {
         return itinerary.getDestination();
     }
+
+    /**
+     * Checks if a given passenger name, origin code, and destination code match the current reservation.
+     *
+     * @param passenger the passenger name to be checked against the current reservation
+     * @param origin the 3-letter airport code for the origin airport to be checked
+     * @param destination the 3-letter airport code for the destination airport to be checked
+     * @return a boolean indicating if the given fields match this reservation
+     */
     public boolean sameReservation(String passenger, String origin, String destination){
         if(getPassenger().equals(passenger) && getOrigin().equals(origin) && getDestination().equals(destination)){
             return true;
--- src/ReservationDB.java
+++ src/ReservationDB.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Holds, deletes, and searches reservations contained in the system.
+ *
  * Created by calvinclark on 10/8/17.
  */
 public class ReservationDB {
@@ -10,6 +12,13 @@ public class ReservationDB {
         reservations = new ArrayList<>();
     }
 
+    /**
+     * Creates a new reservation with a given passenger name and itinerary.
+     *
+     * @param passenger the name of the passenger making the reservation
+     * @param itinerary the itinerary the reservation is being made for
+     * @return a boolean indicating if the reservation is created properly
+     */
     public boolean createReservation(String passenger, Itinerary itinerary){
         Reservation newReservation = new Reservation(passenger, itinerary);
         if (reservations.contains(newReservation)){
@@ -21,6 +30,14 @@ public class ReservationDB {
         }
     }
 
+    /**
+     * Deletes a reservation with a given passenger name, origin airport, and destination airport.
+     *
+     * @param passenger the passenger name on the reservation to be deleted
+     * @param origin the origin airport code on the reservation to be deleted
+     * @param destination the destination airport code on the reservation to be deleted
+     * @return a boolean indicating that the reservation was properly deleted
+     */
     public boolean deleteReservation(String passenger, String origin, String destination) {
         for(int i = 0; i < reservations.size(); i++) {
             Reservation r = reservations.get(i);
@@ -32,6 +49,14 @@ public class ReservationDB {
         return false;
     }
 
+    /**
+     * Searches for a reservation with a given passenger name, origin airport, and destination airport.
+     *
+     * @param passenger the passenger name on the reservation to search for
+     * @param origin the origin airport code on the reservation to search for
+     * @param destination the destination airport code on the reservation to search for
+     * @return a list of all reservations that match the given values
+     */
     public List<Reservation> findReservations(String passenger, String origin, String destination){
         List<Reservation> found = new ArrayList<>();
         for(Reservation r: reservations){
