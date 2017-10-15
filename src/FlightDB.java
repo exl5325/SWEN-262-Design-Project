5273d7868a74525258e46779cc85eae8f42be643
cxc8170 <cxc8170@rit.edu>
2017-10-15 15:53:31 -0400

    Added comments to FlightDB

--- src/FlightDB.java
+++ src/FlightDB.java
@@ -3,17 +3,29 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
+ * Reads and holds all instances of Flight from the specified CSV file, and creates/returns Itineraries using those
+ * flights that match given parameters.
+ *
  * Created by calvinclark on 10/8/17.
  */
 public class FlightDB {
     private List<Flight> flights;
-    private List<Itinerary> itineraries;
     private HashMap<String, Integer> connections = new HashMap<>();
     public FlightDB(){
         readFlights();
         readConnections();
     }
 
+    /**
+     * Creates all possible itineraries that match the given parameters for origin and destination airports,
+     * maximum number of flight connections, and order in which to sort the found itineraries.
+     *
+     * @param origin the 3-letter code for the airport the itinerary departs from
+     * @param destination the 3-letter code for the airport the itinerary ends at
+     * @param numConnections the maximum number of connections (0-2) for the given flight
+     * @param sortOrder a String specifying what the resulting itineraries should be sorted by
+     * @return a list of all possible itineraries that match the given parameters
+     */
     public List<Itinerary> findItineraries(String origin, String destination, int numConnections, String sortOrder){
         List<Itinerary> foundItineraries = new ArrayList<>();
         Comparator<FlightInterface> comparator;
@@ -64,6 +76,10 @@ public class FlightDB {
         return foundItineraries;
     }
 
+    /**
+     * Reads a CSV file named "flights.txt" and creates instances of a Flight object.  Stores those instances in a
+     * List within the class.
+     */
     private void readFlights(){
         flights = new ArrayList<>();
         try {
@@ -87,6 +103,12 @@ public class FlightDB {
         }
     }
 
+    /**
+     * Checks if two flights are able to be connected, meaning that they can be next to one another in an Itinerary.
+     * @param flight1 the earlier flight in the Itinerary
+     * @param flight2 the later flight in the Itinerary
+     * @return a boolean value indicating whether f2 can be placed after f1 in an Itinerary
+     */
     private boolean compatibleFlights(Flight flight1, Flight flight2){
         if(flight1.getDestination() == flight2.getOrigin()){
             String[] a1 = new String[2];
@@ -99,10 +121,20 @@ public class FlightDB {
         return false;
     }
 
+    /**
+     * Checks if a given FlightInterface matches a given origin/destination pair
+     * @param f1 The FlightInterface to be checked
+     * @param origin the requested departure airport
+     * @param destination the requested arrival airport
+     * @return a boolean indicating whether f1 leaves from origin and arrives at destination
+     */
     private boolean correctLocations(FlightInterface f1, String origin, String destination){
         return f1.getOrigin().equals(origin) && f1.getDestination().equals(destination);
     }
 
+    /**
+     * Reads in a CSV file named "connections.txt".  Converts it to a HashMap mapping airport codes to connection times.
+     */
     private void readConnections(){
         try {
             Scanner connectionReader = new Scanner(new File("connections.txt"));
