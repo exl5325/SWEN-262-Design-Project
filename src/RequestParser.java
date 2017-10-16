22838a83d227ec5c8479278bd6b543bccb0ad566
Matt Seaman <mattsdeveloper@gmail.com>
2017-10-15 23:02:46 -0400

    Renamed RequestResponseParser to RequestParser and removed uneccessary methods/properties

--- src/RequestParser.java
+++ src/RequestParser.java
@@ -2,7 +2,7 @@
  * hi
  * Created by peter.audier on 10/8/2017.
  */
-public class RequestResponseParser {
+public class RequestParser {
 
     public String process(String input) { return "Hello"; }
 
