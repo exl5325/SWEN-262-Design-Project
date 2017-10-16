03ff1dacfcf6e4dc91fb1a9f61396902a15ca9a5
Matt Seaman <mattsdeveloper@gmail.com>
2017-10-16 00:09:34 -0400

    Added base request parsing

--- src/Request.java
+++ src/Request.java
@@ -3,6 +3,5 @@
  */
 public interface Request {
 
-
     public Response request();
 }
--- src/RequestParser.java
+++ src/RequestParser.java
@@ -1,16 +1,97 @@
+import java.util.List;
+import java.util.Map;
+
 /**
- * hi
- * Created by peter.audier on 10/8/2017.
+ * Receives text strings and parses them into requests.
+ * Requests are then executed and the responses are returned.
+ *
+ * Created by Matthew Seaman on 10/15/2017.
  */
 public class RequestParser {
 
-    public String process(String input) { return "Hello"; }
+    /**
+     * A coder for reading comma-separated values.
+     */
+    CSVCoder csvCoder = new CSVCoder();
 
-    public Request createRequest(String input){
-        return null;
+    String commandKey = "command";
+
+    String endingValuesKey = "endingValues";
+
+    /**
+     * Processes the input and produces an output.
+     *
+     * @param input: The request string.
+     * @return A non-empty output string.
+     */
+    public String process(String input) {
+        try {
+            Request request = createRequest(input);
+            return request.request().outputData();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
-    public Response createResponse(){
-        return null;
+    /**
+     * Returns a request from the input string.
+     *
+     * @param input: An input string representing a request.
+     * @return A represented request.
+     * @throws Exception: If invalid input was entered. The Exception contains the readable error message.
+     */
+    private Request createRequest(String input) throws Exception {
+        List<Map<String, String>> inputData = csvCoder.readListFromString(input,
+                new String[]{commandKey});
+
+        if (inputData.isEmpty()) {
+            throw new Exception("empty-request");
+        }
+
+        Map<String, String> inputHash = inputData.get(0);
+
+        if (!inputHash.containsKey(commandKey)) {
+            throw new Exception("empty-request");
+        }
+
+        if (!inputHash.containsKey(endingValuesKey)) {
+            throw new Exception("error,unknown request");
+        }
+
+        switch (inputHash.get(commandKey)) {
+            case "info":
+                return flightInfoRequest(inputHash.get(endingValuesKey));
+            case "reserve":
+                return makeReservationRequest(inputHash.get(endingValuesKey));
+            case "retrieve":
+                return getReservationRequest(inputHash.get(endingValuesKey));
+            case "delete":
+                return deleteReservationRequest(inputHash.get(endingValuesKey));
+            case "airport":
+                return airportInfoRequest(inputHash.get(endingValuesKey));
+            default:
+                throw new Exception("error,unknown request");
+        }
     }
+
+    private FlightInfoRequest flightInfoRequest(String input) throws Exception {
+
+    }
+
+    private MakeReservationRequest makeReservationRequest(String input) throws Exception {
+
+    }
+
+    private GetReservationRequest getReservationRequest(String input) throws Exception {
+
+    }
+
+    private DeleteReservationRequest deleteReservationRequest(String input) throws Exception {
+
+    }
+
+    private AirportInfoRequest airportInfoRequest(String input) throws Exception {
+
+    }
+
 }
