2e9824a9b25cabeaf2a3c5a9488ac23425edb344
Matthew Seaman <mattsdeveloper@gmail.com>
2017-11-05 03:37:32 -0500

    Added undo support for DeleteReservationRequest

--- src/RequestResponse/DeleteReservationRequest.java
+++ src/RequestResponse/DeleteReservationRequest.java
@@ -1,6 +1,10 @@
 package RequestResponse;
 
 import Database.DBFacade;
+import Itinerary.Itinerary;
+import Itinerary.Reservation;
+
+import java.util.List;
 
 /**
  * Processes the delete reservation request and creates the proper responses based on if there was an error or not.
@@ -12,6 +16,9 @@ public class DeleteReservationRequest implements Request {
     private String passenger, origin, destination;
     private DBFacade db;
 
+    // will hold itinerary associated with reservation that was deleted
+    private Itinerary itinerary;
+
     public DeleteReservationRequest(String passenger, String origin, String destination, DBFacade db){
         this.passenger = passenger;
         this.origin = origin;
@@ -22,8 +29,29 @@ public class DeleteReservationRequest implements Request {
     // attempts to delete the reservation and returns a response based on if it was successful or not
     @Override
     public Response request() {
-        if(db.deleteReservation(passenger, origin, destination))
+        List<Reservation> reservations = db.findReservations(passenger, origin, destination);
+        if(db.deleteReservation(passenger, origin, destination)) {
+            UndoManager.shared.addRequest(this);
+            itinerary = reservations.get(0).getItinerary();
             return new SimpleResponse("delete,successful");
+        }
         return new SimpleResponse("error,reservation not found");
+    }
+
+    @Override
+    public boolean supportsUndo() {
+        return true;
+    }
+
+    @Override
+    public Response undo() {
+        db.createReservation(passenger, itinerary);
+        return new UndoRedoResponse("undo", "delete", passenger, itinerary);
+    }
+
+    @Override
+    public Response redo() {
+        db.deleteReservation(passenger, origin, destination);
+        return new UndoRedoResponse("redo", "delete", passenger, itinerary);
     }
 }
